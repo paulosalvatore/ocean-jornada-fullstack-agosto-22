bc18d8e045f4b8004ed0a00f51ed2a569c3c648c
Paulo Salvatore <salvatoregames@gmail.com>
2022-08-09 11:59:28 -0300

    Endpoint `/oi` criado

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,12 @@
-console.log("Hello, world!");
+const express = require("express");
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send("Hello, World!");
+});
+
+app.get("/oi", function (req, res) {
+  res.send("Olá, mundo!");
+});
+
+app.listen(3000);
