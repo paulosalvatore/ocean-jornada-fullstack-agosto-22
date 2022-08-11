56c3340e75abf159462dce6afeff6757649fe708
Paulo Salvatore <salvatoregames@gmail.com>
2022-08-11 11:06:52 -0300

    Criação da lista de objetos

--- backend/index.js
+++ backend/index.js
@@ -9,4 +9,25 @@ app.get("/oi", function (req, res) {
   res.send("Olá, mundo!");
 });
 
+// Nosso backend armazena as pontuações das jogadas
+// Criar a lista com as pontuações
+
+const lista = [
+  {
+    id: 1,
+    nome: "Paulo",
+    pontos: 21,
+  },
+  {
+    id: 2,
+    nome: "Daniel",
+    pontos: 52,
+  },
+  {
+    id: 3,
+    nome: "Beatriz",
+    pontos: 97,
+  },
+];
+
 app.listen(3000);
