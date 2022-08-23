893ea2ed7f369cbe21b63f3d624713b6deca70b8
Paulo Salvatore <salvatoregames@gmail.com>
2022-08-23 10:42:39 -0300

    Criação do estado `estaPulando` e detecção do clique (WIP)

--- frontend/src/components/Jogo/Jogo.js
+++ frontend/src/components/Jogo/Jogo.js
@@ -2,8 +2,33 @@ import "./Jogo.css";
 import nuvens from "../../assets/clouds.png";
 import cano from "../../assets/pipe.png";
 import mario from "../../assets/mario.gif";
+import { useState } from "react";
 
 function Jogo() {
+  /*
+  const estaPulando = useState(false);
+  const estado = estaPulando[0];
+  const dispatch = estaPulando[1];
+
+  // Desconstrução de array
+  // const lista = [10, 20, 30];
+  // const [numero1, numero2, numero3] = lista;
+  const [numero1, numero2, numero3] = [10, 20, 30];
+  */
+
+  // Criamos o estado `estaPulando`, com o valor padrão `false`.
+  // Primeiro valor é apenas para ler
+  // Segundo valor é para atualizar o estado
+  const [estaPulando, setEstaPulando] = useState(false);
+
+  document.onkeydown = function () {
+    console.log("On Key Down");
+
+    estaPulando = true;
+  };
+
+  console.log(15, { estaPulando });
+
   return (
     <div className="jogo">
       <img className="nuvens" src={nuvens} alt="Nuvens" />
