596e8031b51a2665399b7240b885e2d1234b93d1
Paulo Salvatore <salvatoregames@gmail.com>
2022-08-23 12:09:33 -0300

    Lógica de GameOver adicionada (WIP)

--- frontend/src/components/Jogo/Jogo.js
+++ frontend/src/components/Jogo/Jogo.js
@@ -2,7 +2,7 @@ import "./Jogo.css";
 import nuvens from "../../assets/clouds.png";
 import cano from "../../assets/pipe.png";
 import mario from "../../assets/mario.gif";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 function Jogo() {
   /*
@@ -22,6 +22,39 @@ function Jogo() {
   // No momento que um estado é atualizado, o componente atualiza
   // tudo o que está sendo renderizado
   const [estaPulando, setEstaPulando] = useState(false);
+
+  // Criamos as referências para `mario` e `cano`
+  const marioRef = useRef();
+  const canoRef = useRef();
+
+  function marioEstaNoCano() {
+    // Acessamos as referências do mario e do cano
+    const mario = marioRef.current;
+    const cano = canoRef.current;
+
+    // Se por acaso `mario` ou `cano` não forem encontrados,
+    // encerra essa função
+    if (!mario || !cano) {
+      return;
+    }
+
+    // Retorna o valor da lógica que determinar se o mário
+    // está na mesma posição do cano ou não (com as checagens
+    // que consideram toda a área do cano)
+    return (
+      cano.offsetLeft > mario.offsetLeft &&
+      cano.offsetLeft < mario.offsetLeft + mario.offsetWidth &&
+      mario.offsetTop + mario.offsetHeight > cano.offsetTop
+    );
+  }
+
+  // Implementação temporária para exibir se o mário está no cano
+  // ou não
+  setInterval(function () {
+    const valor = marioEstaNoCano();
+
+    console.log("Mário está no cano?", valor);
+  }, 100);
 
   document.onkeydown = function () {
     // Atualizamos o estado para true
@@ -47,9 +80,9 @@ function Jogo() {
     <div className="jogo">
       <img className="nuvens" src={nuvens} alt="Nuvens" />
 
-      <img className="cano" src={cano} alt="Cano" />
+      <img ref={canoRef} className="cano" src={cano} alt="Cano" />
 
-      <img className={marioClassName} src={mario} alt="Mário" />
+      <img ref={marioRef} className={marioClassName} src={mario} alt="Mário" />
 
       <div className="chao"></div>
     </div>
