fc6dc6c500422ee7c336fe84ed7db145b49b9ef1
Paulo Salvatore <salvatoregames@gmail.com>
2022-08-31 11:26:21 -0300

    Criação da tela `HighScore`

--- frontend/src/components/HighScore/HighScore.js
+++ frontend/src/components/HighScore/HighScore.js
@@ -1,7 +1,29 @@
 import "./HighScore.css";
 
 function HighScore() {
-  return <div>HighScore</div>;
+  return (
+    <div className="HighScore">
+      <div>
+        Você fez <b>50</b> pontos!
+      </div>
+
+      <div>
+        <h1>HighScore</h1>
+
+        <div>Paulo - 90 pontos</div>
+        <div>João - 50 pontos</div>
+        <div>Ana - 32 pontos</div>
+      </div>
+
+      <div>
+        <h1>Registre sua pontuação!</h1>
+        <form>
+          <input type="text" placeholder="Digite o seu nome..." />
+          <input type="submit" value="Enviar" />
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default HighScore;
